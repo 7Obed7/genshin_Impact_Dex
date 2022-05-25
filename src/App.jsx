abe5889d6c926254e8157d3dd3071185fd4d1c6e
Lauro Obed Nolasco Vazquez <isc18350511@gmail.com>
2022-05-25 15:57:55 -0500

    Añadiendo nuevo select

--- src/App.jsx
+++ src/App.jsx
@@ -19,27 +19,47 @@ const App = () => {
     types: [],
   });
 
-  const fetchTypes = async () => {
-    const respuesta = await fetchHelper("https://api.genshin.dev/");
-    const { types } = await respuesta.json();
-    console.log({ types });
-    setSelectets({ ...selects, types });
+  const fetchTypes = async (url, item) => {
+    const respuestaJson = await fetchHelper(url);
+    const respuesta = await respuestaJson.json();
+
+    if (item === "types") {
+      setSelectets({ ...selects, [item]: respuesta[item] });
+    } else {
+      setSelectets({ ...selects, [item]: respuesta });
+    }
+    console.log(selects);
   };
 
   useEffect(() => {
-    fetchTypes().catch(console.error);
+    fetchTypes("https://api.genshin.dev/", "types").catch(console.error);
   }, []);
 
+  const handleChangeType = ({ target }) => {
+    fetchTypes(`https://api.genshin.dev/${target.value}`, target.value);
+  };
   return (
     <div className="container">
       <h1>Genshin Impact Dex</h1>
       <hr />
-      <select>
-        <option value="">Seleccione el tipo de información</option>
+      <select onChange={handleChangeType}>
+        <option value="">Seleccione el tipo de material</option>
         {selects.types.map((type) => (
-          <option value={type}>{tipos[type]}</option>
+          <option value={type} key={type}>
+            {tipos[type]}
+          </option>
         ))}
       </select>
+      {selects.materials && (
+        <select name="materials">
+          <option value="">Seleccione un artefacto</option>
+          {selects.materials.map((material) => (
+            <option value={material} key={material}>
+              {material}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 };
